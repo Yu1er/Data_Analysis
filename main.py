34adc4fb6ee3071489ce3e74159a29d03275741e
Yuer <84618101+Yu1er@users.noreply.github.com>
2023-07-30 16:00:01 +0800

    完成doc所有分析 + 重删窗口计算

--- main.py
+++ main.py
@@ -84,9 +84,77 @@ def noref_hit_lpnupdate_count():
         writef.write(str(key) + ' ' + str(noref_hit_lpnupdate[key]) + '\n')
 
 
+def cold_in_hit_count():
+    cold_in_hit_times = {}  # 统计 以cold-lpn写入后命中的次数 的个数，即比如 以cold-lpn写入 且 命中4次的 md码有几个
+    for key in cold_in_hit:
+        nums = cold_in_hit[key]
+        if nums in cold_in_hit_times:
+            cold_in_hit_times[nums] = cold_in_hit_times[nums] + 1
+        else:
+            cold_in_hit_times[nums] = 1
+    sorted_key = sorted(cold_in_hit_times)
+    # print(sorted_key)
+    write_file = 'cold_in_hit_num' + x + '.txt'
+    writef = open(write_file, 'w')
+    writef = open(write_file, 'a')
+    for key in sorted_key:
+        writef.write(str(key) + ' ' + str(cold_in_hit_times[key]) + '\n')
+    print('以cold-LPN写入的数据:' + str(len(cold_in_md5) * 8.0 / 2 / 1024 / 1024) + 'GB\n')
+    print('以cold-LPN写入且命中的数据:' + str(len(cold_in_hit) * 8.0 / 2 / 1024 / 1024) + 'GB\n')
+
+
+def cold_hot_percent(p):
+    hlpn_hmd5, clpn_hmd5, hlpn_cmd5, clpn_cmd5 = 0, 0, 0, 0
+    for key in lpn_to_md5:
+        data = lpn_to_md5[key]
+        if lpn[key] <= int(lpn_thresh):
+            if md5_ref_live[data] <= int(md5_thresh):
+                clpn_cmd5 = clpn_cmd5 + 1
+            else:
+                clpn_hmd5 = clpn_hmd5 + 1
+        else:
+            if md5_ref_live[data] <= int(md5_thresh):
+                hlpn_cmd5 = hlpn_cmd5 + 1
+            else:
+                hlpn_hmd5 = hlpn_hmd5 + 1
+    # for key in md5_to_lpn:
+    #     data = md5_to_lpn[key]
+    #     if md5_n[key] <= int(md5_thresh):
+    #         if lpn[data] <= int(lpn_thresh):
+    #             clpn_cmd5 = clpn_cmd5 + 1
+    #         else:
+    #             hlpn_cmd5 = hlpn_cmd5 + 1
+    #     else:
+    #         if lpn[data] <= int(lpn_thresh):
+    #             clpn_hmd5 = clpn_hmd5 + 1
+    #         else:
+    #             hlpn_hmd5 = hlpn_hmd5 + 1
+    sum0 = clpn_cmd5 + clpn_hmd5 + hlpn_cmd5 + hlpn_hmd5
+    write_file = 'cold_hot_percent_' + x + '.txt'
+    writef = open(write_file, 'a')
+    writef.write('At ' + p + '\n')
+    writef.write('hot_lpn-hot_md5:' + str(hlpn_hmd5) + '  percent:' + str(hlpn_hmd5 * 1.0 / sum0) + '\n')
+    writef.write('cold_lpn-hot_md5:' + str(clpn_hmd5) + '  percent:' + str(clpn_hmd5 * 1.0 / sum0) + '\n')
+    writef.write('hot_lpn-cold_md5:' + str(hlpn_cmd5) + '  percent:' + str(hlpn_cmd5 * 1.0 / sum0) + '\n')
+    writef.write('cold_lpn-cold_md5:' + str(clpn_cmd5) + '  percent:' + str(clpn_cmd5 * 1.0 / sum0) + '\n\n')
+
+
+def rehit_durtime_count():
+    sorted_key = sorted(rehit_durtime)
+    # print(sorted_key)
+    write_file = 'rehit_durtime' + x + '.txt'
+    writef = open(write_file, 'w')
+    writef = open(write_file, 'a')
+    for key in sorted_key:
+        writef.write(str(key) + ' ' + str(rehit_durtime[key]) + '\n')
+
+
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    x = str(input())
+    x = str(input('file:'))
+    open('cold_hot_percent_' + x + '.txt', 'w')
+    lpn_thresh = input('lpn_thresh:')
+    md5_thresh = input('md5_thresh:')
     file_name = '../traces/cheetah.cs.fiu.edu-110108-113008.' + x + '.blkparse'
     # 记录原始数据的lba、siz、typ、md5
     lba, siz, typ, md5 = [], [], [], []
@@ -94,8 +162,12 @@ if __name__ == '__main__':
     w_num, r_num, w_siz, r_siz, w_uniq, r_uniq, sam = 0, 0, 0, 0, 0, 0, 0
     # 每个lpn被更新的次数、每个md5码在写请求中要求写入的次数、lpn到md5的映射表、md5码实时的引用次数、md5码的引用次数变为0的时间
     lpn, md5_n, lpn_to_md5, md5_ref_live, md5_noref_time = {}, {}, {}, {}, {}
-    # "未被引用的数据"又被命中时与它成为未被引用数据的间隔、有哪些md5码成为"未被引用的数据"后又被命中、md5到lpn的映射表、未被引用数据重新命中时其原来的lpn的更新次数
+    # "未被引用的数据"又被命中时与它成为未被引用数据的间隔、有哪些md5码成为"未被引用的数据"后又被命中、md5成为未引用数据前最后一次写入的lpn、未被引用数据重新命中时md5之前最后一个对应的lpn的更新次数
     noref_hit_durtime, noref_hit_md5, md5_to_lpn, noref_hit_lpnupdate = {}, {}, {}, {}
+    # 首次以cold_LPN写入的md5码、以cold-lpn写入并被hit的次数(变为未引用数据前)
+    cold_in_md5, cold_in_hit, hot_in_md5 = {}, {}, {}
+    # 相同数据上一次写入的时候的时间、两个重复数据间写入了多少数据
+    rehit_time, rehit_durtime = {}, {}
     # 写入过但是此时未被引用的数据又被命中的次数、总共产生过的“未被引用的数据”
     noref_hit_times, noref_num = 0, 0
     # 写入过的lba、读取过的lba、读写过的lba
@@ -123,28 +195,47 @@ if __name__ == '__main__':
                         noref_hit_md5[md5_now] = noref_hit_md5[md5_now] + 1
                     else:
                         noref_hit_md5[md5_now] = 1
+
                     dt = wi - md5_noref_time[md5_now]
                     if dt in noref_hit_durtime:
                         noref_hit_durtime[dt] = noref_hit_durtime[dt] + 1
                     else:
                         noref_hit_durtime[dt] = 1
-                    old_lpn = md5_to_lpn[md5_now]
-                    old_lpn_update = lpn[old_lpn]
-                    if old_lpn_update in noref_hit_lpnupdate:
-                        noref_hit_lpnupdate[old_lpn_update] = noref_hit_lpnupdate[old_lpn_update] + 1
+
+                    last_lpn = md5_to_lpn[md5_now]
+                    last_lpn_update = lpn[last_lpn]
+                    if last_lpn_update in noref_hit_lpnupdate:
+                        noref_hit_lpnupdate[last_lpn_update] = noref_hit_lpnupdate[last_lpn_update] + 1
                     else:
-                        noref_hit_lpnupdate[old_lpn_update] = 1
+                        noref_hit_lpnupdate[last_lpn_update] = 1
 
                 md5_ref_live[md5_now] = md5_ref_live[md5_now] + 1
-                md5_n[md5[i]] = md5_n[md5[i]] + 1
+                md5_n[md5_now] = md5_n[md5_now] + 1
                 sam = sam + siz[i]
+
+                dt = wi - rehit_time[md5_now]
+                rehit_time[md5_now] = wi
+                if dt in rehit_durtime:
+                    rehit_durtime[dt] = rehit_durtime[dt] + 1
+                else:
+                    rehit_durtime[dt] = 1
+
+                if (md5_now in cold_in_md5) and (
+                        md5_now not in md5_noref_time):  # 如果当前的md5码在这个md5_noref_time中说明曾经成为过未引用数据
+                    if md5_now in cold_in_hit:
+                        cold_in_hit[md5_now] = cold_in_hit[md5_now] + 1
+                    else:
+                        cold_in_hit[md5_now] = 1
             else:
                 md5_n[md5_now] = 1
                 md5_ref_live[md5_now] = 1
                 w_uniq = w_uniq + siz[i]
+                rehit_time[md5_now] = wi  # 第一次写入时记录写入的时间
 
             lpn_b = int(lba_b / 8)
             lpn_e = int(lba_e / 8)
+            if md5_now not in md5_to_lpn:
+                md5_to_lpn[md5_now] = lpn_b  # 如果该md5只在第一次写入时被引用，则这个lpn就是成为未引用数据前的最后一个lpn
             for j in range(lpn_b, lpn_e):
                 if j in lpn:
                     lpn[j] = lpn[j] + 1
@@ -153,12 +244,15 @@ if __name__ == '__main__':
                         if md5_ref_live[md5_now] == 0:
                             noref_num = noref_num + 1
                             md5_noref_time[md5_now] = wi  # 成为未被引用的数据的时间
+                            md5_to_lpn[md5_now] = lpn_b  # md5在成为未引用数据前的最后一个对应的lpn
                 else:
                     lpn[j] = 1
                 lpn_to_md5[j] = md5_now
-            md5_to_lpn[md5_now] = lpn_b
             if lpn_b == lpn_e:
                 lpn[lpn_b] = lpn[lpn_b] + 1
+
+            if (md5_n[md5_now] == 1) and (lpn[lpn_b] <= lpn_thresh):  # 以cold-lpn写入
+                cold_in_md5[md5_now] = 1
 
         else:
             r_num = r_num + 1
@@ -168,6 +262,15 @@ if __name__ == '__main__':
                 wr_lba.add(j)
             if md5[i] in md5_n and md5_n[md5[i]] == 1:
                 r_uniq = r_uniq + siz[i]
+
+        if i == num / 4:
+            cold_hot_percent('25%')
+        elif i == num / 2:
+            cold_hot_percent('50%')
+        elif i == num / 4 * 3:
+            cold_hot_percent('75%')
+        elif i == num - 1:
+            cold_hot_percent('100%')
 
     write_file = 'res_' + x + '.txt'
     writef = open(write_file, 'w')
@@ -193,12 +296,16 @@ if __name__ == '__main__':
 
     writef.write('负载重复数据的大小：' + str(sam * 1.0 / 2 / 1024 / 1024) + ' GB \n\n\n')
 
-    writef.write('产生过的"未被引用的数据"(一个md5码多次变为"未被引用的数据"则多次计数)：' + str(noref_num * 1.0 / 2 / 1024 / 1024) + ' GB \n')
-    writef.write('"未被引用的数据"被用于重删：' + str(noref_hit_times * 1.0 / 2 / 1024 / 1024) + ' GB \n')
+    writef.write(
+        '产生过的"未被引用的数据"(一个md5码多次变为"未被引用的数据"则单次计数)：' + str(len(md5_noref_time) * 8.0 / 2 / 1024 / 1024) + ' GB \n')
+    writef.write('产生过的"未被引用的数据"(一个md5码多次变为"未被引用的数据"则多次计数)：' + str(noref_num * 8.0 / 2 / 1024 / 1024) + ' GB \n')
+    writef.write('"未被引用的数据"被用于重删：' + str(noref_hit_times * 8.0 / 2 / 1024 / 1024) + ' GB \n')
     writef.write('"未被引用的数据"被用于重删的几率("未被引用的数据"被用于重删/产生过的"未被引用的数据")：' + str(noref_hit_times * 1.0 / noref_num) + ' \n')
 
-    lpn_update_times()
-    md5_count()
-    noref_rehit_count()
-    noref_hit_durtime_count()
-    noref_hit_lpnupdate_count()
+    # lpn_update_times()
+    # md5_count()
+    # noref_rehit_count()
+    # noref_hit_durtime_count()
+    # noref_hit_lpnupdate_count()
+    # cold_in_hit_count()
+    rehit_durtime_count()
